dee138b8c65b6690c59c6a97d4d55602b59c0cd7
Zhang Sheng <shengzhang90@gmail.com>
2014-11-26 20:58:05 +0800

    Update BubbleSort.cpp

--- Sort/BubbleSort.cpp
+++ Sort/BubbleSort.cpp
@@ -1,5 +1,5 @@
 void BubbleSort(int a[], int len){
-	for(int i = 0; i < len - 1; i--){
+	for(int i = 0; i < len - 1; i++){
 		for(int j = len - 1; j > i && a[j-1] > a[j]; j--){
 			swap(a, j-1, j);
 		}
